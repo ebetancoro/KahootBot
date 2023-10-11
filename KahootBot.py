af3470cd39fe41169116b9b1bce5cce42663063a
TheHxin <hxin.radin@gmail.com>
2023-10-11 19:16:35 +0330

    Question & Answer Detection Added

--- KahootBot.py
+++ KahootBot.py
@@ -7,19 +7,49 @@ driver.get("https://kahoot.it/")
 
 def EnterGame():
     driver.find_element("id" , "game-input").send_keys(input("Enter Gamecode: "))
-    driver.find_element("id" ," game-input").submit()
+    driver.find_element("id" , "game-input").submit()
 
     while driver.title.__contains__("PIN"):
         continue
     
     driver.find_element("id","nickname").send_keys(input("Enter Nickname: "))
     driver.find_element("id","nickname").submit()
+
 def DetectAnswers():
-    answer0 = driver.find_element("css selector" , "['']")
-    answer1
-    answer2
-    answer3
+    answer0 = driver.find_element("css selector" , "[data-functional-selector='question-choice-text-0']")
+    answer1 = driver.find_element("css selector" , "[data-functional-selector='question-choice-text-1']")
+    answer2 = driver.find_element("css selector" , "[data-functional-selector='question-choice-text-2']")
+    answer3 = driver.find_element("css selector" , "[data-functional-selector='question-choice-text-3']")
 
-EnterGame()
+    return [answer0,answer1,answer2,answer3]
 
-input()
+def DetectQuestion():
+    question = driver.find_element("css selector" , "[data-functional-selector='block-title']")
+
+    return question
+
+def DoRound():
+    answers = DetectAnswers()
+    question = DetectQuestion()
+
+    TestDetetctionFunctions(answers,question)
+
+def TestDetetctionFunctions(answers,question):
+    for i in answers:
+        print(i.text)
+    print(question.text)
+
+def Start():
+    EnterGame()
+    
+    input("Start the game? ")
+    
+    while True:
+        DoRound()
+        i = input("Next Round? (y/n)")
+        if i == "n":
+            break
+
+Start()
+
+input("Thanks for using Kahoot-God service ... ")
